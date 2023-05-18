9ef162f1a2f8cd498149ea8122d55e4cfde254e8
Maksim Efremov <efremovmax2002@gmail.com>
2023-05-18 20:47:30 +0500

    feat: add showing modal on movieListItem cllick

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren } from "react";
 
 import styles from './Modal.module.css';
+import { useOutsideClick } from "../../hooks/useOutsideClick";
 
 type Props = {
   active: boolean;
@@ -8,12 +9,13 @@ type Props = {
 }
 
 const Modal: FC<PropsWithChildren<Props>> = ({ active, onClose, children }) => {
-
+  const ref = useOutsideClick(onClose);
+  
   if (!active) return null;
-
+  
   return (
     <div className={styles.backdrop}>
-      <div className={styles.modal}>
+      <div className={styles.modal} ref={ref}>
         <button
           className={styles.closeButton}
           onClick={onClose} />
--- client/src/components/MovieListItem/MovieListItem.tsx
+++ client/src/components/MovieListItem/MovieListItem.tsx
@@ -1,37 +1,53 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { TMovie } from '../../models/TMovie';
 import styles from './MovieListItem.module.css';
+import Modal from '../Modal/Modal';
+import MovieCard from '../MovieCard/MovieCard';
 
 type Props = {
   movie: TMovie;
 }
 
 const MovieListItem: FC<Props> = ({ movie }) => {
+  const [modalActive, setModalActive] = useState(false);
+  const modalOnClose = () => setModalActive(false);
+
   return (
-    <div className={styles.movie}>
-      <img
-        className={styles.poster}
-        src={movie.poster}
-        alt="Movie poster" />
-      <div className={styles.info}>
-        <div className={styles.header}>
-          <h2 className={styles.name}>{movie.name}</h2>
-          <div className={styles.altName}>{movie.alternativeName}</div>
-        </div>
-        <div className={styles.footer}>
-          <div>
-            <div className={styles.footerInfo}>
-              {`${movie.year}, ${movie.countries.join(', ')}`}
-            </div>
-            <div className={styles.footerInfo}>
-              {movie.genres.join(', ')}
-            </div>
+    <>
+      <div className={styles.movie} onClick={() => { setModalActive(true) }}>
+        <img
+          className={styles.poster}
+          src={movie.poster}
+          alt="Movie poster" />
+        <div className={styles.info}>
+          <div className={styles.header}>
+            <h2 className={styles.name}>{movie.name}</h2>
+            <div className={styles.altName}>{movie.alternativeName}</div>
           </div>
-          <button className={styles.replaceButton} />
+          <div className={styles.footer}>
+            <div>
+              <div className={styles.footerInfo}>
+                {`${movie.year}, ${movie.countries.join(', ')}`}
+              </div>
+              <div className={styles.footerInfo}>
+                {movie.genres.join(', ')}
+              </div>
+            </div>
+            <button
+              className={styles.replaceButton}
+              onClick={(e) => {
+                e.stopPropagation();
+                alert('Редактировать');
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <Modal active={modalActive} onClose={modalOnClose}>
+        <MovieCard movie={movie} />
+      </Modal>
+    </>
   );
 }
 
